04a18785eaf44a004a78f6d31f34925d2342bc92
sharan036 <sharangopal36@gmail.com>
2023-06-19 13:04:27 +0530

    updated files

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
-const path = require('path');
+// const path = require('path');
 
+// const express = require('express');
+
+// const app = express();
+
+
+// const db = require('./database/database');
+
+// app.set('views', path.join(__dirname, 'views'))
+// app.use(express.static('public'))
+// app.set('view engine', 'ejs');
+// app.use(express.urlencoded({ extented: false }))
+
+// app.get('/', function (req, res) {
+//     res.render('index');
+// })
+// app.post('/', async function (req, res) {
+//     const message = {
+//         name: req.body.name,
+//         email:req.body.email,
+//         userMessage:req.body.message
+//     }
+//     const insertMessage = await db.getDb().collection('message').insertOne(message);
+//     console.log('Data Inserted')
+//     res.redirect('/api');
+// })
+// db.connectToDatabase().then(
+//     app.listen(3000, () => console.log(`app listen at port 3000`))
+// )
 const express = require('express');
-
+const path = require('path')
 const app = express();
-
-
+// const bodyParser = require('body-parser')
 const db = require('./database/database');
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+const cors = require("cors")
 app.use(express.urlencoded({ extented: false }))
-app.use(express.static('public'))
+// const fetch = require("node-fetch");
+const port = 3000
+// app.use(bodyParser)
+app.use(cors())
+app.set('views',path.join(__dirname,'views'))
+app.use(express.static(path.join(__dirname, "public")));
+app.set('view engine','ejs')
 
-app.get('/', function (req, res) {
-    res.render('index');
+app.get('/',(req,res)=>{
+    res.render('index')
 })
 app.post('/', async function (req, res) {
     const message = {
@@ -26,6 +57,7 @@ app.post('/', async function (req, res) {
     res.redirect('/api');
 })
 db.connectToDatabase().then(
-    app.listen(3000)
-
+    app.listen(port, () => console.info(`App listening on port ${port}`))
 )
+
+
